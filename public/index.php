f8ebb0e3178c5a0c0894db7eeeed1a20bc9d658e
Hanna <Hannamyr.bd@gmail.com>
2021-04-21 17:21:17 +0300

    add docker

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
-тут фронт контроллер - одна точка входа
+<!-- тут фронт контроллер - одна точка входа
 тут у тебя и сессия стартует и так далее.
-тут ты и будешь парсить урл что бы понять какой открывать
+тут ты и будешь парсить урл что бы понять какой открывать -->
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   P
+</body>
+</html>
